1380151344b43f3de995fbc5470974a991cb851e
Ihor <ihor.chanzhar@gmail.com>
2025-08-19 16:03:21 +0300

    Update GenericRsqlSpecBuilder.kt

--- src/main/kotlin/com/github/ichanzhar/rsql/GenericRsqlSpecBuilder.kt
+++ src/main/kotlin/com/github/ichanzhar/rsql/GenericRsqlSpecBuilder.kt
@@ -8,43 +8,44 @@ import org.springframework.data.jpa.domain.Specification
 
 class GenericRsqlSpecBuilder<E>(val distinct: Boolean) {
 
-	fun createSpecification(node: Node): Specification<E>? {
-		if (node is LogicalNode) {
-			return createSpecification(node)
-		}
-		if (node is ComparisonNode) {
-			return createSpecification(node)!!
-		}
-		return null
-	}
+    fun createSpecification(node: Node): Specification<E>? {
+        if (node is LogicalNode) {
+            return createSpecification(node)
+        }
+        if (node is ComparisonNode) {
+            return createSpecification(node)!!
+        }
+        return null
+    }
 
-	fun createSpecification(logicalNode: LogicalNode): Specification<E> {
-		val specs: MutableList<Specification<E>> = logicalNode.children
-			.mapNotNull { createSpecification(it) }
-			.toMutableList()
+    fun createSpecification(logicalNode: LogicalNode): Specification<E> {
+        val specs: MutableList<Specification<E>> = logicalNode.children
+            .mapNotNull { createSpecification(it) }
+            .toMutableList()
 
-		var result: Specification<E> = specs[0]
-		if (logicalNode.operator == LogicalOperator.AND) {
-			for (i in 1 until specs.size) {
-				result = Specification.where(result).and(specs[i])
-			}
-		} else if (logicalNode.operator == LogicalOperator.OR) {
-			for (i in 1 until specs.size) {
-				result = Specification.where(result).or(specs[i])
-			}
-		}
-		return result
-	}
 
-	fun createSpecification(comparisonNode: ComparisonNode): Specification<E>? {
-		return Specification.where(
-			JpaRsqlSpecification<E>(
-				comparisonNode.selector,
-				comparisonNode.operator,
-				comparisonNode.arguments,
-				distinct
-			)
-		)
-	}
+        var result: Specification<E> = specs[0]
+        if (logicalNode.operator == LogicalOperator.AND) {
+            for (i in 1 until specs.size) {
+                result = result.and(specs[i])
+            }
+        } else if (logicalNode.operator == LogicalOperator.OR) {
+            for (i in 1 until specs.size) {
+                result = result.or(specs[i])
+            }
+        }
+        return result
+    }
+
+    fun createSpecification(comparisonNode: ComparisonNode): Specification<E>? {
+        return Specification<E> { root, query, criteriaBuilder ->
+            JpaRsqlSpecification<E>(
+                comparisonNode.selector,
+                comparisonNode.operator,
+                comparisonNode.arguments,
+                distinct
+            ).toPredicate(root, query, criteriaBuilder)
+        }
+    }
 
 }
